136087ad049ded28a164701479395a6846a38bff
BenEdwards-0612 <112256392+BenEdwards-0612@users.noreply.github.com>
2023-03-21 20:23:04 +0000

    menu work (it does now)

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        canvas.gameObject.SetActive(false);
+        SceneManager.LoadScene("ParallaxPlatformer", LoadSceneMode.Single);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
